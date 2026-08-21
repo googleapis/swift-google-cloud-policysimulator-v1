bfeb4f1c0b5b0f1fae5b5b5c3880feace3db8bcb
Carlos O'Ryan <coryan@google.com>
2026-08-21 21:36:45 +0000

    chore: update librarian circa 2028-08-21 (#507)
    This version uses `_QueryParameterEncoder()` which unblocks the removal
    of `QueryParameterEncoder()`.
    
    It also imports `GoogleCloudGax` with `@_spi()` so we can protect
    `GoogleCloudGax._RetryLoop`.

--- Sources/GoogleCloudPolicySimulatorV1/OrgPolicyViolationsPreviewService+Transport.swift
+++ Sources/GoogleCloudPolicySimulatorV1/OrgPolicyViolationsPreviewService+Transport.swift
@@ -46,7 +46,7 @@ extension Clients {
       var query = [
         URLQueryItem(name: "$alt", value: "json;enum-encoding=int")
       ]
-      let encoder = GoogleCloudGax.QueryParameterEncoder()
+      let encoder = GoogleCloudGax._QueryParameterEncoder()
       query.append(contentsOf: try encoder.encode(request.pageSize, prefix: "pageSize"))
       query.append(contentsOf: try encoder.encode(request.pageToken, prefix: "pageToken"))
       var req = try await self.inner.newRequest(path: path, query: query)
@@ -91,7 +91,7 @@ extension Clients {
       var query = [
         URLQueryItem(name: "$alt", value: "json;enum-encoding=int")
       ]
-      let encoder = GoogleCloudGax.QueryParameterEncoder()
+      let encoder = GoogleCloudGax._QueryParameterEncoder()
       query.append(
         contentsOf: try encoder.encode(
           request.orgPolicyViolationsPreviewId, prefix: "orgPolicyViolationsPreviewId"))
@@ -118,7 +118,7 @@ extension Clients {
       var query = [
         URLQueryItem(name: "$alt", value: "json;enum-encoding=int")
       ]
-      let encoder = GoogleCloudGax.QueryParameterEncoder()
+      let encoder = GoogleCloudGax._QueryParameterEncoder()
       query.append(contentsOf: try encoder.encode(request.pageSize, prefix: "pageSize"))
       query.append(contentsOf: try encoder.encode(request.pageToken, prefix: "pageToken"))
       var req = try await self.inner.newRequest(path: path, query: query)
@@ -142,7 +142,7 @@ extension Clients {
       var query = [
         URLQueryItem(name: "$alt", value: "json;enum-encoding=int")
       ]
-      let encoder = GoogleCloudGax.QueryParameterEncoder()
+      let encoder = GoogleCloudGax._QueryParameterEncoder()
       query.append(contentsOf: try encoder.encode(request.filter, prefix: "filter"))
       query.append(contentsOf: try encoder.encode(request.pageSize, prefix: "pageSize"))
       query.append(contentsOf: try encoder.encode(request.pageToken, prefix: "pageToken"))
